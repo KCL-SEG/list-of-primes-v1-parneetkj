cfc5c6ff58a678614c947a1f873ea2e84bef9f50
Parneet Johal <parneetkjohal@gmail.com>
2022-10-01 14:21:46 +0100

    Add while loop to return list of inputted number of prime numbers

--- primes.py
+++ primes.py
@@ -3,4 +3,13 @@
 
 def primes(number_of_primes):
     list = []
+    i = 0
+    while len(list) < number_of_primes:
+        if i > 1:
+            for j in range(2, i):
+                if (i % j) == 0:
+                    break
+            else:
+                list.append(i)
+        i += 1
     return list
